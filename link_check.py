f472f5b6fbce65071e44a069dbf95ca91f0696f4
James Tuttle <summer@braggtown.com>
2023-05-16 08:23:42 -0400

    Adds sleep delay to avoid triggering download restrictions

--- link_check.py
+++ link_check.py
@@ -2,12 +2,27 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 from argparse import ArgumentParser
+from time import sleep
+
+
+def validateSleepArg(seconds):
+    try:
+        int(seconds)
+    except ValueError:
+        return False
+    if int(seconds) in range(0, 60, 1):
+        return True
+    else:
+        print("Sleep delay must an integer be between 0 and 60 seconds.")
+        exit()
 
 
 def main():
     parser = ArgumentParser()
     parser.add_argument("-v", "--verbose", help="increase output verbosity",
                         action="store_true")
+    parser.add_argument("-d", "--delay", help="seconds to sleep between reqeuests (integer)",
+                        action="store")
     args = parser.parse_args()
 
     # Uses the current date and time to add to the output filename so you
@@ -15,6 +30,13 @@ def main():
     now = datetime.now()
     date_time = now.strftime("%m%d%Y-%H%M%S")
     filename= 'url_check_results'+date_time+'.txt'
+
+    if args.delay:
+        if validateSleepArg(args.delay):
+            print(args.delay , "second delay between requests")
+        else:
+            print("Argument Error.  Sleep delay must be an integer (1, 2, etc).")
+            exit()
 
     # Read URLs from the 'urls.txt' file
     with open('urls.txt', 'r') as urls_file:
@@ -48,6 +70,8 @@ def main():
             f.write(record + "\n")
             if args.verbose:
                 print(record)
+            if args.delay:
+                sleep(int(args.delay))
 
 
 if __name__=="__main__":
